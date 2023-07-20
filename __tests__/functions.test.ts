b3a6c89ed2da71d8b94ba2a4da40fa51364e3cbb
geyser-digitalfemsa <139982172+geyser-digitalfemsa@users.noreply.github.com>
2023-07-20 16:00:45 -0500

    paso: verde

--- __tests__/functions.test.ts
+++ __tests__/functions.test.ts
@@ -11,4 +11,8 @@ test("multiply function multiplies two numbers correctly", () => {
 // Add more test cases for the other functions
 test("Fibonacci de un número", () => {
   expect(fibonacci(7)).toBe(13);
+  expect(fibonacci(88)).toBe(1100087778366101931);
+  expect(fibonacci(31)).toBe(1346269);
+  expect(fibonacci(4)).toBe(3);
+  expect(fibonacci(22)).toBe(17711);
 });
--- functions.ts
+++ functions.ts
@@ -8,5 +8,11 @@ export function multiply(a: number, b: number): number {
    
 // Add more functions as needed
 export function fibonacci(num: number): number {
-    return -1;
+    const sol = [0, 1];
+
+    for (let index = 2; index<= num; index++) {
+        sol[index] = sol[index -1] + sol[index - 2];
+    }
+
+    return sol[num];
 }
