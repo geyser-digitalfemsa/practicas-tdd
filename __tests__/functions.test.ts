2d8e372882a5179bf3c754ff6a43b93d824612bf
geyser-digitalfemsa <139982172+geyser-digitalfemsa@users.noreply.github.com>
2023-07-20 15:35:41 -0500

    ejemplo del lab

---
+++ __tests__/functions.test.ts
@@ -0,0 +1,11 @@
+import { capitalize, multiply } from "../functions";
+ 
+test("capitalize function capitalizes the first letter of a word", () => {
+  expect(capitalize("hello")).toBe("Hello");
+});
+ 
+test("multiply function multiplies two numbers correctly", () => {
+  expect(multiply(2, 3)).toBe(6);
+});
+ 
+// Add more test cases for the other functions
